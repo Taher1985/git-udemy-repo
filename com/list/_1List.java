fc135cc4df6c8d741a4ccc96bedbc6580e166a66
Taher <taher@ali.org>
2022-07-11 06:31:42 +0530

    add java collection

---
+++ com/list/_1List.java
@@ -0,0 +1,13 @@
+package com.list;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class _1List {
+
+	public void listOperation() {
+		List<String> names = Arrays.asList("A", "B");
+		System.out.println(names);
+	}
+
+}
---
+++ com/set/_3Set.java
@@ -0,0 +1,15 @@
+package com.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _3Set {
+
+	public void setOperation() {
+		Set<String> names = new HashSet<>();
+		names.add("A");
+		names.add("B");
+		System.out.println(names);
+	}
+
+}
